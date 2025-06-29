5a83e29cbbd87018460a7aff3bfbfff263a7f171
thinkableweb <designcabinauckland@gmail.com>
2025-06-30 09:18:31 +1200

    Update color-change.js

--- color-change.js
+++ color-change.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const colors = [
         '#E83C00',  // Orange
         '#0d00ff',  // Blue
-        '#510296',  // Purple
+        '#3b007a',  // Purple
         '#D10000',  // Red
         '#000000'   // Black
     ];
